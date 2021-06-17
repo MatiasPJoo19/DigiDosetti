c0beb7ca7771be4a802686576ce8bdc6c930c090
MatiasPJoo19 <58026139+MatiasPJoo19@users.noreply.github.com>
2021-06-17 10:53:21 +0300

    Lääkeen lisäys activity lisätty

---
+++ app/src/main/java/sovellus/digidosetti/LaakenLisaysActivity.java
@@ -0,0 +1,14 @@
+package sovellus.digidosetti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaakenLisaysActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laaken_lisays);
+    }
+}
