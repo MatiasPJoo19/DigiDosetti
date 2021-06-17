b6108d6a7a7f7a3c6878a80115cd73f71a4c4f7d
MatiasPJoo19 <58026139+MatiasPJoo19@users.noreply.github.com>
2021-06-17 11:14:37 +0300

    Lisätty animaatiot ja button setappia

--- app/src/main/java/sovellus/digidosetti/MedicinesicActivity.java
+++ app/src/main/java/sovellus/digidosetti/MedicinesicActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,11 +24,22 @@ public class MedicinesicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_medicinesic);
 
         BottomNavigationView  bottomNavigationView = findViewById(R.id.alavalikko);
-
+        Button Lisaalaake = findViewById(R.id.buttonLisaaLaake);
         //ensimmäinen sivu valittu
         // tämä on home sivu
 
         bottomNavigationView.setSelectedItemId(R.id.medicinesic);
+
+
+        Lisaalaake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LisaaLaakeVoid();
+            }
+        });
+
+
+
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -58,5 +71,10 @@ public class MedicinesicActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    public void LisaaLaakeVoid(){
+        startActivity(new Intent(getApplicationContext()
+                                , LaakenLisaysActivity.class));     //tässä valitaan mikä activity avataan
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 }
