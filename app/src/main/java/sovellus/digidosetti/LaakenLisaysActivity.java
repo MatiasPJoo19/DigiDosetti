6732d12cd144c2b53c270ba5e77b98bca3513a86
MatiasPJoo19 <58026139+MatiasPJoo19@users.noreply.github.com>
2021-06-17 23:46:56 +0300

    Lisätty animaatiot

--- app/src/main/java/sovellus/digidosetti/LaakenLisaysActivity.java
+++ app/src/main/java/sovellus/digidosetti/LaakenLisaysActivity.java
@@ -2,7 +2,9 @@ package sovellus.digidosetti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LaakenLisaysActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LaakenLisaysActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laaken_lisays);
+    }
+    public void openActivity3(View view) {
+        Intent intent = new Intent(this, MedicinesicActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
